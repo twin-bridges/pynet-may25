fec20a502f1605b2076c942a83b347382b681538
Kirk Byers <ktbyers@twb-tech.com>
2025-05-04 15:04:10 -0700

    Lesson1, Sb, ex6

---
+++ lesson1/quick_exercises/sectionb_ex5.py
@@ -0,0 +1,5 @@
+# Create a Python script that prompts you for your address and then prints out this
+# response. Use Python to execute your script.
+
+address = input("Enter your address: ")
+print(address)
