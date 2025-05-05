626e7d81ec314f872c4d9972bbd5bb184c026eab
Kirk Byers <ktbyers@twb-tech.com>
2025-05-05 13:44:26 -0700

    SectionE, ex1

--- lesson1/quick_exercises/sectione/exercise1.py
+++ lesson1/quick_exercises/sectione/exercise1.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 """
-Open a file
-Read a file using f.read()
-Read a file using f.readlines()
+a. Open a file named "some_file.txt" for reading. This file will need to be in the same
+   directory as your program and have some text in it. Read this file using the '.read()'
+   method. Make sure the file is properly closed. Print the file contents out to standard
+   output.
+
+b. Open the file named "some_file.txt" for reading again. This time read the file using the
+   .readlines() method. Print the results from .readlines() out to standard output (this
+   should be a list that contains all the lines of the file).
+
 """
+
+# Part "a" ####
 
 # No context manager
 f = open("some_file.txt")
@@ -12,6 +20,9 @@ data = f.read()
 f.close()
 
 print(f"\nEx1a:\n\n{data}\n\n")
+
+
+# Part "b" ####
 
 # Using a context manager
 with open("some_file.txt") as f:
