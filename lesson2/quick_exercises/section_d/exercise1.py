972996876adf253a332dafea0edde776fdc53ad4
Kirk Byers <ktbyers@twb-tech.com>
2025-05-07 15:00:12 -0700

    Cl2, SectD, Ex1

---
+++ lesson2/quick_exercises/section_d/exercise1.py
@@ -0,0 +1,21 @@
+"""
+Create a Python script that does the following:
+a. Contains a simple function with no parameters that executes three print statements and
+   returns 42
+
+b. Call this function three times and verify your code executes properly.
+
+"""
+
+
+# Part a.
+def my_func():
+    print("Hello")
+    print("World")
+    print("And Something Else.\n")
+
+
+# Part b. (actually call the function three times)
+my_func()
+my_func()
+my_func()
