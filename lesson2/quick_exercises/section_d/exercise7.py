e8ca1a659f2856909b25e1adc6a1152f4c4da973
Kirk Byers <ktbyers@twb-tech.com>
2025-05-08 13:53:27 -0700

    Cl2, SectD, Ex7

---
+++ lesson2/quick_exercises/section_d/exercise7.py
@@ -0,0 +1,26 @@
+"""
+
+Create a Python script that reuses your function from exercise 2:
+
+a. Call this function using positional arguments except use the *args technique.
+
+"""
+
+
+# Function from Ex. 2
+def my_func(x, y, z):
+    print(f"{x = }")
+    print(f"{y = }")
+    print(f"{z = }")
+
+
+my_args = [7, 8, 42]
+
+# Call with positional arguments except using *args
+print("\nFunc call: 'my_func(*args)':")
+my_func(*my_args)
+
+# Also works if my_args is a tuple:
+my_args = (7, 8, 99)
+print("\nFunc call: 'my_func(*args)': (tuple)")
+my_func(*my_args)
