dbbf1bd6470242af44ab8cd1c9a54e94f0d79ff8
Kirk Byers <ktbyers@twb-tech.com>
2025-06-04 20:15:54 -0700

    Ex9

--- lesson4/quick_exercises/section_c/exercise9.py
+++ lesson4/quick_exercises/section_c/exercise9.py
@@ -2,38 +2,57 @@ import os
 from rich import print
 from netmiko import ConnectHandler
 
-password = os.environ["NETMIKO_PASSWORD"]
 
-vmx2 = {
-    "device_type": "juniper_junos",
-    "host": "vmx2.lasthop.io",
-    "username": "pyclass",
-    "password": password,
-}
+def find_current_dns(ssh_conn):
+    """
+    Find the currently configured DNS servers on a Junos Device.
 
-name_servers = [
-    "1.1.1.1",
-    "1.0.0.1",
-]
+    Return a list of name-servers.
+    """
+    current_ns = ssh_conn.send_command("show configuration system name-server")
+    current_ns = current_ns.splitlines()
+    dns_servers = []
+    for ns in current_ns:
+        ns = ns.strip()
+        # Strip off trailing ';'
+        ns = ns.rstrip(";")
+        dns_servers.append(ns)
 
-base_add_cmd = "set system name-server"
-base_delete_cmd = "delete system name-server"
-commands = []
-for ns in name_servers:
-    cmd = f"{base_add_cmd} {ns}"
-    commands.append(cmd)
+    return dns_servers
 
-ssh_conn = ConnectHandler(**vmx2)
-current_nservers = ssh_conn.send_command("show configuration system name-server")
-current_nservers = current_nservers.splitlines()
-for ns in current_nservers:
-    ns = ns.strip()
-    # Strip off trailing ';'
-    ns = ns.rstrip(";")
-    # Delete any extra name-servers
-    if ns not in name_servers:
-        cmd = f"{base_delete_cmd} {ns}"
+
+if __name__ == "__main__":
+
+    password = os.environ["NETMIKO_PASSWORD"]
+
+    vmx2 = {
+        "device_type": "juniper_junos",
+        "host": "vmx2.lasthop.io",
+        "username": "pyclass",
+        "password": password,
+    }
+
+    name_servers = [
+        "1.1.1.1",
+        "1.0.0.1",
+    ]
+
+    base_add_cmd = "set system name-server"
+    base_delete_cmd = "delete system name-server"
+    commands = []
+    for ns in name_servers:
+        cmd = f"{base_add_cmd} {ns}"
         commands.append(cmd)
 
-print(commands)
-ssh_conn.disconnect()
+    ssh_conn = ConnectHandler(**vmx2)
+    current_nservers = find_current_dns(ssh_conn)
+    # Delete any extra name-servers
+    for ns in current_nservers:
+        if ns not in name_servers:
+            cmd = f"{base_delete_cmd} {ns}"
+            commands.append(cmd)
+
+    output = ssh_conn.send_config_set(commands)
+    output += ssh_conn.commit()
+    print(output)
+    ssh_conn.disconnect()
