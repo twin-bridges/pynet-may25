c97fe1f882c5a1ab5e57cc617263a176fdbb5cfd
Kirk Byers <ktbyers@twb-tech.com>
2025-06-14 09:10:27 -0700

    First pass at J2 env exercise

---
+++ lesson5/exercises/j2_ex1.py
@@ -0,0 +1,25 @@
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2 import Environment
+
+env = Environment(undefined=StrictUndefined)
+env.loader = FileSystemLoader([".", "./templates/"])
+
+local_bgp_defs = {
+    "bgp_as": 65000,
+    "bgp_router_id": "1.1.1.1",
+}
+cust1_ref_id = "cust422"
+customer_bgp_defs = {
+    "customer_reference": cust1_ref_id,
+    "customer_bgp_peer_group": f"{cust1_ref_id.upper()}-PEER-GROUP",
+    "customer_bgp_as": 64999,
+    "customer_bgp_neighbor_ip": "172.31.254.1",
+}
+j2_vars = local_bgp_defs
+j2_vars.update(customer_bgp_defs)
+print(j2_vars)
+
+template_file = "sros_bgp_ex1.j2"
+template = env.get_template(template_file)
+output = template.render(**j2_vars)
+print(output)
