4f792d3658605303453bb852485a5d201ab2cb7b
Kirk Byers <ktbyers@twb-tech.com>
2025-06-14 12:23:42 -0700

    Class5, Jinja2 Exercises

--- lesson5/exercises/j2_ex2.py
+++ lesson5/exercises/j2_ex2.py
@@ -12,13 +12,15 @@ cust1_ref_id = "cust422"
 customer_bgp_defs = {
     "customer_reference": cust1_ref_id,
     "customer_bgp_peer_group": f"{cust1_ref_id.upper()}-PEER-GROUP",
+    "customer_bgp_afi_ipv4": True,
+    "customer_bgp_afi_ipv6": True,
     "customer_bgp_as": 64999,
     "customer_bgp_neighbor_ip": "172.31.254.1",
 }
 j2_vars = local_bgp_defs
 j2_vars.update(customer_bgp_defs)
 
-template_file = "sros_bgp_ex1.j2"
+template_file = "sros_bgp_ex2.j2"
 template = env.get_template(template_file)
 output = template.render(**j2_vars)
 print(output)
