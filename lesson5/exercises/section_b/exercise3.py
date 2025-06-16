26c111a4ba0e6394bb0c543a11a8f2e17a7ad7b0
Kirk Byers <ktbyers@twb-tech.com>
2025-06-15 21:23:54 -0700

    Class5, sectb, exercise3

--- lesson5/exercises/section_b/exercise3.py
+++ lesson5/exercises/section_b/exercise3.py
@@ -1,12 +1,13 @@
 from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2 import Environment
+from rich import print
 
 env = Environment(undefined=StrictUndefined)
 env.loader = FileSystemLoader([".", "./templates/"])
 
 local_bgp_defs = {
-    "bgp_as": 65000,
-    "bgp_router_id": "1.1.1.1",
+    "bgp_local_as": 64500,
+    "bgp_router_id": "10.1.1.1",
 }
 cust1_ref_id = "cust422"
 customer_bgp_defs = {
@@ -14,7 +15,7 @@ customer_bgp_defs = {
     "customer_bgp_peer_group": f"{cust1_ref_id.upper()}-PEER-GROUP",
     "customer_bgp_afi_ipv4": True,
     "customer_bgp_afi_ipv6": True,
-    "customer_bgp_as": 64999,
+    "customer_bgp_as": 64511,
     "customer_bgp_neighbors": ["172.31.254.1", "172.31.7.1"],
 }
 j2_vars = local_bgp_defs
@@ -23,4 +24,6 @@ j2_vars.update(customer_bgp_defs)
 template_file = "sros_bgp_ex3.j2"
 template = env.get_template(template_file)
 output = template.render(**j2_vars)
+print()
 print(output)
+print()
